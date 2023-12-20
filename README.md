7ca04456c7364cb0f861e30a0f10861e2a89c10c
Sebastian Rath <git@sebastianrath.io>
2023-12-20 00:58:23 -0500

    🚀 Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+![Actionforge Banner](https://www.actionforge.dev/assets/social.jpg?)
+
+# Actionforge Action
+
+Welcome to the Actionforge Action source code!
+
+Actionforge introduces Action Graphs, a faster and easier way to create and execute GitHub workflows. This new approach replaces tedious YAML file editing by hand with a user-friendly graph editor. Action Graphs are compatible with GitHub Actions. The core product consists out of 4 components:
+
+- ♾️ [VS Code Extension](https://www.github.com/actionforge/vscode-ext) (to be made public) - Extension to modify Action Graphs within VS Code.
+- 🟢 [GitHub Action](https://www.github.com/actionforge/action) - GitHub Action that reads the Action Graph and starts the Graph Runner with it.
+- 🏃‍♀️ [Graph Runner](https://www.github.com/actionforge/graph-runner) (to be made public) - The cli program that executes an Action Graph.
+- 🕸️ [Graph Editor](https://www.github.com/actionforge/graph-editor) (to be made public) - Visual graph editor to create and build Action Graphs. These graphs will be committed to your repository.
+
+For a full introduction check out the [Actionforge Documentation](https://www.actionforge.dev/docs).
+
+## Usage
+
+<!-- start usage -->
+```yaml
+- uses: actionforge/action@v0.4.35
+  with:
+    # The name of the graph file located in the `.github/workflows/graphs` directory.
+    # This file defines the Actionforge Action Graph to be executed.
+    graph_file: ''
+
+    # A Personal Access Token (PAT) used for authentication. 
+    # If not provided, the default GitHub token for the repository will be used.
+    # Default: ${{ github.token }}
+    #
+    # [Learn more about creating and using encrypted secrets](https://help.github.com/en/actions/automating-your-workflow-with-github-actions/creating-and-using-encrypted-secrets)
+    token: ''
+```
+
+For examples check out the [Actionforge Examples](https://www.github.com/actionforge/examples) 🔗 repository.
+
+## License
+
+This SOFTWARE is licensed under the Actionforge Community License that you can find [here](https://github.com/actionforge/legal/blob/main/LICENSE.md).
+
+Licenses for commercial use will soon be available on the GitHub Marketplace.
+
+For further information [Get in touch](mailto:hello@actionforge.dev).
---
+++ src/main.ts
@@ -0,0 +1,143 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import * as stream from "stream";
+import * as util from "util";
+import * as tc from "@actions/tool-cache";
+
+import os from "os";
+import path from "path";
+import fs from "fs";
+import got from "got";
+import cp from "child_process";
+import pj from './bundled_package.json';
+
+/**
+ * Represents information about a runner version,
+ * including its download URL, resolved version, and file name.
+ */
+export interface IRunnerVersionInfo {
+  downloadUrl: string;
+  resolvedVersion: string;
+  filename: string;
+}
+
+/**
+ * Extracts the contents of an archive file to a directory.
+ * @param archivePath The path to the archive file.
+ * @returns A Promise that resolves to the path of the extracted directory.
+ */
+export async function extractArchive(archivePath: string): Promise<string> {
+  const platform = os.platform();
+  return platform === "win32"
+    // Windows requires the .zip extension for extraction
+    ? tc.extractZip(archivePath + '.zip')
+    : tc.extractTar(archivePath);
+}
+
+/**
+ * Downloads the specified runner version and returns the path to the executable.
+ * @param info - The runner version information.
+ * @returns The path to the executable.
+ */
+async function downloadRunner(info: IRunnerVersionInfo): Promise<string> {
+  const token = core.getInput("token");
+  if (!token) {
+      throw new Error(`No GitHub token found`)
+  }
+
+  const tempDir = process.env.RUNNER_TEMP || '.';
+  const filename = path.join(tempDir, info.filename);
+
+  const pipeline = util.promisify(stream.pipeline);
+  await pipeline(
+    got.stream(info.downloadUrl, {
+      method: "GET",
+      headers: {
+        "User-Agent": "GitHub Actions",
+        Accept: "application/octet-stream",
+        Authorization: `token ${token}`,
+      },
+    }),
+    fs.createWriteStream(filename)
+  );
+
+  const extPath = await extractArchive(filename);
+  const execPath = path.join(extPath, info.filename);
+  fs.chmodSync(execPath, 0o755);
+
+  return execPath;
+}
+
+/**
+ * Executes a runner with the specified runnerPath and sets up the required environment variables.
+ * @param runnerPath The path to the runner executable.
+ * @param graphFile The path to the graph file.
+ * @returns A Promise that resolves when the runner has finished executing.
+ */
+async function executeRunner(
+  runnerPath: string,
+  graphFile: string,
+): Promise<void> {
+  const token = core.getInput("token");
+
+  const octokit = github.getOctokit(token);
+  const { data } = await octokit.rest.repos.getContent({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    ref: github.context.sha,
+    path: graphFile,
+  });
+
+  fs.mkdirSync(path.dirname(graphFile), { recursive: true });
+  const buf = Buffer.from((data as { content: string }).content, "base64");
+  fs.writeFileSync(graphFile, buf.toString("utf-8"));
+
+  const customEnv = { ...process.env, GRAPH_FILE: graphFile };
+  cp.execSync(runnerPath, { stdio: "inherit", env: customEnv });
+}
+
+/**
+ * The main function that downloads and installs the runner.
+ */
+async function run(): Promise<void> {
+  const baseUrl = `https://github.com/actionforge/${pj.name}/releases/download`;
+  const downloadUrl = `${baseUrl}/v${pj.version}/graph-runner-${os.platform()}-${os.arch()}-${pj.version}.tar.gz`;
+  console.log("Downloading runner from", downloadUrl);
+
+  const downloadInfo = {
+    downloadUrl: downloadUrl,
+    resolvedVersion: pj.version,
+    filename: 'graph-runner',
+  };
+
+  const GRAPH_FILE_DIR = ".github/workflows/graphs";
+  const graphFile = path.join(
+    GRAPH_FILE_DIR,
+    core.getInput("graph_file", { required: true })
+  );
+
+  // Log output
+  const refName = process.env.GITHUB_REF_NAME;
+  const sha = process.env.GITHUB_SHA;
+  const repo = process.env.GITHUB_REPOSITORY;
+  const runId = process.env.GITHUB_RUN_ID;
+  const output = `🟢 View Action Graph: https://www.actionforge.dev/github/${repo}/${refName ?? sha}/${graphFile}?run_id=${runId}`;
+  const delimiter = '-'.repeat(32);
+  console.log(`${delimiter}`);
+  console.log(output);
+  console.log(`${delimiter}`);
+
+  const runnerPath = await downloadRunner(downloadInfo);
+  executeRunner(runnerPath, graphFile);
+}
+
+async function main(): Promise<void> {
+  try {
+    await run();
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message);
+    return process.exit(1);
+  }
+}
+
+main();
